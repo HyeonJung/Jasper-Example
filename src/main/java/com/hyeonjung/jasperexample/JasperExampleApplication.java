decc90fdb0f0195c32eafe577ae609ba4381e9da
윤현중 <guswnd1592@gaeasoft.co.kr>
2018-11-01 11:12:19 +0900

    first commit

---
+++ src/main/java/com/hyeonjung/jasperexample/JasperExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hyeonjung.jasperexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hyeonjung/jasperexample/JasperExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyeonjung.jasperexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JasperExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
